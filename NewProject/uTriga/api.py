95d99323dcef77f986e14798972f70048d0ce44a
Dede Ankrah <pcspida@ubuntu.(none)>
2012-07-30 17:28:04 +0000

    api changes

--- NewProject/uTriga/api.py
+++ NewProject/uTriga/api.py
@@ -5,13 +5,16 @@ class UserResource(ModelResource):
     class Meta:
         queryset = User.objects.all()
         resource_name = 'user'
+        allowed_methods = ['get']
 
 class EventResource(ModelResource):
     class Meta:
         queryset = Event.objects.all()
         resource_name = 'event'
+        allowed_methods = ['get']
 
 class CategoryResource(ModelResource):
     class Meta:
         queryset = Category.objects.all()
         resource_name = 'category'
+        allowed_methods = ['get']
--- NewProject/uTrigaPro/urls.py
+++ NewProject/uTrigaPro/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
+from tastypie.api import Api
 from uTriga.api import UserResource,EventResource,CategoryResource 
 
-event_resource = EventResource()
-user_resource = UserResource()
-category_resource = CategoryResource()
+v1_api = Api(api_name='utriga')
+v1_api.register(UserResource())
+v1_api.register(EventResource())
+v1_api.register(CategoryResource())
 
 
 # Uncomment the next two lines to enable the admin:
@@ -19,8 +21,6 @@ url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
 	{'document_root' : settings.STATIC_ROOT,}),
 url(r'^media/(?P<path>.*)$', 'django.views.static.serve', 
 	{'document_root' : settings.MEDIA_ROOT,}),
-(r'^event/', include(event_resource.urls)),
-(r'^user/', include(user_resource.urls)),
-(r'^category/', include(category_resource.urls)),
+(r'^api/', include(v1_api.urls)),
                                              
 )
