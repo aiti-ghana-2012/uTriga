8fef1baa9c22868cdaf812ae0aa466b1849fb52d
elrapha <elrapha@elrapha-pc.(none)>
2012-07-31 16:04:39 +0000

    reducing size

--- NewProject/reg/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('reg.views',
-    url(r'^login/$', 'do_login'),
-    url(r'^logout/$', 'do_logout'),
-    url(r'^sign_up/$', 'sign_up'),
-)
